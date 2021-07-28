22b7d0e83aaa00ccb44d835efef6d2505c26690d
bildzeitung <bildzeitung@gmail.com>
2021-07-27 23:20:16 -0400

    Add project files.

---
+++ CP380-B2-BlockWebAPI/Controllers/PendingPayloadsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CP380_B2_BlockWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class PendingPayloadsController : ControllerBase
+    {
+        // TODO
+    }
+}
---
+++ CP380-B2-BlockWebAPI/Models/PendingPayloads.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CP380_B2_BlockWebAPI.Models
+{
+    public class PendingPayloads
+    {
+        // TODO
+    }
+}
